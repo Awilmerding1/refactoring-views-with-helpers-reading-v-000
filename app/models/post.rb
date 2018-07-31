9cbd883fe165d8dd6d9d7b2adb3fbdb66c00bd0e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-31 14:45:41 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,8 @@ class Post < ActiveRecord::Base
   private
   
   def last_updated
-  updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
-end
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
